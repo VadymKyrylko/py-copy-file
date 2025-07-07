bb7fa6a1915b7f984310c2d915e0ce9266727e50
Vadym <kirilkovadim193@gmail.com>
2025-07-07 16:09:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    names_of_files = command.split()
+    if len(names_of_files) < 3 or names_of_files[0] != "cp":
+        return
+    if names_of_files[1] == names_of_files[2]:
+        return
+    origin = names_of_files[1]
+    new_copy = names_of_files[2]
+    try:
+        with (open(origin, "r") as info_in,
+              open(new_copy, "w") as info_out):
+            content = info_in.read()
+            info_out.write(content)
+    except FileNotFoundError:
+        return
